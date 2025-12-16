ab34142b4e0233559df2cc8f58c845bb69cd87e5
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-12-16 13:45:43 +0300

    .

--- src/main/java/com/tugalsan/api/email/server/TS_EMailConvertUtils.java
+++ src/main/java/com/tugalsan/api/email/server/TS_EMailConvertUtils.java
@@ -1,5 +1,7 @@
 package com.tugalsan.api.email.server;
 
+import com.tugalsan.api.log.server.TS_Log;
+
 import module com.tugalsan.api.function;
 import module com.tugalsan.api.union;
 import module org.simplejavamail;
@@ -7,18 +9,27 @@ import java.nio.file.*;
 
 public class TS_EMailConvertUtils {
 
+    final private static TS_Log d = TS_Log.of(TS_EMailConvertUtils.class);
+
     private TS_EMailConvertUtils() {
 
     }
 
-    public static TGS_UnionExcuseVoid convertMSG2EML(Path pathInputMsg, Path pathOutputEml) {
-        return TGS_FuncMTCUtils.call(() -> {
-            String emlString = EmailConverter.outlookMsgToEML(pathInputMsg.toFile());
-            try (var writer = Files.newBufferedWriter(pathOutputEml)) {
-                writer.write(emlString);
-            }
-            return TGS_UnionExcuseVoid.ofVoid();
-        }, e -> TGS_UnionExcuseVoid.ofExcuse(e));
-    }
+//    @Deprecated //NOT WORKING
+//    public static TGS_UnionExcuseVoid convertMSG2EML(Path pathInputMsg, Path pathOutputEml) {
+//        return TGS_FuncMTCUtils.call(() -> {
+//            d.cr("convertMSG2EML", "#0");
+//            System.setProperty("org.simplejavamail.debug", "true");
+//            var emlString = EmailConverter.outlookMsgToEML(pathInputMsg.toFile());
+//            d.cr("convertMSG2EML", "#1");
+//            try (var writer = Files.newBufferedWriter(pathOutputEml)) {
+//                d.cr("convertMSG2EML", "#2");
+//                writer.write(emlString);
+//                d.cr("convertMSG2EML", "emlString", emlString);
+//            }
+//            d.cr("convertMSG2EML", "#4");
+//            return TGS_UnionExcuseVoid.ofVoid();
+//        }, e -> TGS_UnionExcuseVoid.ofExcuse(e));
+//    }
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,10 @@
 module com.tugalsan.api.email {
     requires jakarta.activation;
     requires jakarta.mail;
-    requires org.simplejavamail;
-    requires org.simplejavamail.core;
-    requires org.simplejavamail.outlook;
+//    requires org.simplejavamail;
+//    requires org.simplejavamail.core;
+//    requires org.simplejavamail.smime;
+//    requires org.simplejavamail.outlook;
     requires com.tugalsan.api.string;
     requires com.tugalsan.api.union;    
     requires com.tugalsan.api.function;
